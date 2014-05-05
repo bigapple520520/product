a01880892027ef3d6e2b49e7850662a3d88dd0aa
xuan <15858178400@163.com>
2014-05-05 16:57:35 +0800

    20140505

---
+++ yibacontent/yiba_youmi/src/com/xuan/app/yiba/Main.java
@@ -0,0 +1,205 @@
+package com.xuan.app.yiba;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.ClipboardManager;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.xuan.app.yiba.slidingupdown.SlidingUpDown;
+import com.xuan.app.yiba.slidingupdown.SlidingUpDown.OpenPercentListener;
+import com.xuan.app.yiba.utils.Utils;
+
+import net.youmi.android.AdManager;
+import net.youmi.android.banner.AdSize;
+import net.youmi.android.banner.AdView;
+
+/**
+ * 翻译吧主界面
+ * 
+ * @author xuan
+ * @version $Revision: 1.0 $, $Date: 2014-5-5 下午1:45:55 $
+ */
+public class Main extends Activity {
+    private Button button; // 走一个按钮
+    private EditText fromWordEditText;// 输入
+    private TextView toWordTextView;// 输出
+    private TextView explain;
+    private TextView explainWord;
+
+    private final Handler handler = new Handler();
+    private ProgressDialog progressDialog;
+
+    private SlidingUpDown slidingUpDown;
+    private View aboveView;
+    private View behindView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.main);
+
+        // 上下滑动
+        initSliding();
+
+        // 有米广告嵌入
+        initYouMi();
+
+        button = (Button) aboveView.findViewById(R.id.button);
+        fromWordEditText = (EditText) aboveView.findViewById(R.id.fromWord);
+        toWordTextView = (TextView) aboveView.findViewById(R.id.toWord);
+        explain = (TextView) aboveView.findViewById(R.id.explain);
+        explainWord = (TextView) aboveView.findViewById(R.id.explainWord);
+
+        progressDialog = new ProgressDialog(this);
+
+        explainWord.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (TextUtils.isEmpty(explainWord.getText().toString())) {
+                    return false;
+                }
+
+                ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                cm.setText(explainWord.getText().toString());
+                Toast.makeText(Main.this, "亲，别的解释已复制了哦，去别地黏贴吧", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        toWordTextView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                if (TextUtils.isEmpty(toWordTextView.getText().toString())) {
+                    return false;
+                }
+
+                ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                cm.setText(toWordTextView.getText().toString());
+                Toast.makeText(Main.this, "亲，译文已复制了哦，去别地黏贴吧", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 判断网络
+                if (!Utils.hasNetwork(Main.this)) {
+                    Toast.makeText(Main.this, "亲，网络不可用哦", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // 隐藏键盘输入
+                InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(
+                        Context.INPUT_METHOD_SERVICE);
+                if (imm.isActive()) {
+                    imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+                }
+
+                // 处理翻译
+                final String fromWord = fromWordEditText.getEditableText().toString();
+
+                if (TextUtils.isEmpty(fromWord)) {
+                    Toast.makeText(Main.this, "输入内容不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Utils.showDialog("我在奋力翻译中...", progressDialog);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            final List<String> explainsList = new ArrayList<String>();
+                            final String toWord = Utils.getTranslateWord(fromWord, explainsList);// 查询有道翻译API
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    explain.setText("");
+                                    explainWord.setText("");
+
+                                    toWordTextView.setText(toWord);
+
+                                    // 显示解释
+                                    if (!explainsList.isEmpty()) {
+                                        explain.setText("一些别的解释：");
+                                        StringBuilder sb = new StringBuilder();
+                                        for (String explainWord : explainsList) {
+                                            sb.append(explainWord).append("，");
+                                        }
+
+                                        explainWord.setText(sb.toString().substring(0, sb.length() - 1));
+                                    }
+                                }
+                            });
+                        }
+                        finally {
+                            Utils.dismissDialog(handler, progressDialog);
+                        }
+                    }
+                }).start();
+            }
+        });
+    }
+
+    // 向上滑动
+    private void initSliding() {
+        aboveView = LayoutInflater.from(this).inflate(R.layout.above, null);
+        behindView = LayoutInflater.from(this).inflate(R.layout.behind, null);
+
+        // 设置侧滑界面参数
+        slidingUpDown = (SlidingUpDown) findViewById(R.id.slidingUpDown);
+        slidingUpDown.setAboveContent(aboveView);
+        slidingUpDown.setBehindContent(behindView);
+
+        // 参数配置
+        slidingUpDown.setMode(SlidingUpDown.MODE_UP);
+
+        slidingUpDown.setFadeEnabled(true);
+        slidingUpDown.setFadeDegree(0.99f);
+
+        // 设置打开的渐变
+        slidingUpDown.setOpenPercentListener(new OpenPercentListener() {
+            @Override
+            public void openPercent(float percent) {
+                int padding = (int) (40 * Math.abs(1 - percent)) + 5;
+                behindView.setPadding(padding, padding, padding, padding);
+            }
+        });
+    }
+
+    // 初始化有米广告
+    private void initYouMi() {
+        // 发布ID，密钥，调试模式
+        AdManager.getInstance(Main.this).init("23a727717cb74a5b", "62a3e6960adbca32", false);
+
+        // 底部广告
+        LinearLayout adLayout = (LinearLayout) aboveView.findViewById(R.id.adLayout);
+        AdView adView = new AdView(Main.this, AdSize.FIT_SCREEN);
+        adLayout.addView(adView);
+
+        // 统计用户
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                AdManager.getInstance(Main.this).setUserDataCollect(true);
+            }
+        }).start();
+    }
+
+}
---
+++ yibacontent/yiba_youmi/src/com/xuan/app/yiba/slidingupdown/BehindView.java
@@ -0,0 +1,143 @@
+/* 
+ * @(#)BehindView.java    Created on 2014-1-8
+ * Copyright (c) 2014 ZDSoft Networks, Inc. All rights reserved.
+ * $Id$
+ */
+package com.xuan.app.yiba.slidingupdown;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 背面拉出来的View
+ * 
+ * @author xuan
+ * @version $Revision: 1.0 $, $Date: 2014-1-8 下午7:48:16 $
+ */
+public class BehindView extends ViewGroup {
+	private SlidingUpDown slidingUpDown;
+
+	private View mContent;// 从下出现的view
+	private AboveView mAboveView;
+
+	private final Paint mFadePaint = new Paint();
+	private float mFadeDegree;
+	private boolean mFadeEnabled;
+
+	public BehindView(Context context) {
+		this(context, null);
+	}
+
+	public BehindView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public BehindView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int width = getDefaultSize(0, widthMeasureSpec);
+		int height = getDefaultSize(0, heightMeasureSpec);
+		setMeasuredDimension(width, height);
+
+		final int contentWidthMeasureSpec = getChildMeasureSpec(
+				widthMeasureSpec, 0, width);
+		final int contentHeightMeasureSpec = getChildMeasureSpec(
+				heightMeasureSpec, 0, height);
+		mContent.measure(contentWidthMeasureSpec, contentHeightMeasureSpec);
+	}
+
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		final int width = r - l;
+		final int height = b - t;
+		mContent.layout(0, 0, width, height);
+	}
+
+	public void setContent(View view) {
+		if (null != mContent) {
+			removeView(mContent);
+		}
+		mContent = view;
+		addView(mContent);
+	}
+
+	public View getContent() {
+		return mContent;
+	}
+
+	public View getAboveView() {
+		return mAboveView;
+	}
+
+	public void setAboveView(AboveView above) {
+		this.mAboveView = above;
+	}
+
+	public SlidingUpDown getSlidingUpDown() {
+		return slidingUpDown;
+	}
+
+	public void setSlidingUpDown(SlidingUpDown slidingUpDown) {
+		this.slidingUpDown = slidingUpDown;
+	}
+
+	/**
+	 * 根据比例在content绘制遮罩层渐变
+	 * 
+	 * @param content
+	 *            AboveView对象
+	 * @param canvas
+	 * @param openPercent
+	 */
+	public void drawFade(View content, Canvas canvas, float openPercent) {
+		if (!mFadeEnabled) {
+			return;
+		}
+
+		final int alpha = (int) (mFadeDegree * 255 * Math.abs(1 - openPercent));
+		mFadePaint.setColor(Color.argb(alpha, 0, 0, 0));
+
+		int top = 0;
+		int bottom = 0;
+		if (slidingUpDown.getMode() == SlidingUpDown.MODE_UP) {
+			top = content.getBottom();
+			bottom = content.getBottom() + getHeight();
+		} else if (slidingUpDown.getMode() == SlidingUpDown.MODE_DOWN) {
+			top = content.getTop() - getHeight();
+			bottom = content.getTop();
+		} else if (slidingUpDown.getMode() == SlidingUpDown.MODE_UP_DOWN) {
+			top = content.getBottom();
+			bottom = content.getBottom() + getHeight();
+			canvas.drawRect(0, top, getWidth(), bottom, mFadePaint);
+
+			top = content.getTop() - getHeight();
+			bottom = content.getTop();
+		}
+		canvas.drawRect(0, top, getWidth(), bottom, mFadePaint);
+	}
+
+	public float getFadeDegree() {
+		return mFadeDegree;
+	}
+
+	public void setFadeDegree(float fadeDegree) {
+		this.mFadeDegree = fadeDegree;
+	}
+
+	public boolean isFadeEnabled() {
+		return mFadeEnabled;
+	}
+
+	public void setFadeEnabled(boolean fadeEnabled) {
+		this.mFadeEnabled = fadeEnabled;
+	}
+
+}
